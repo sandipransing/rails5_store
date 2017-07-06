8beca769aea2a43ab62681ff04d0e20c6c17cddb
sandip ransing <sandip@funonrails.com>
2017-07-06 18:49:14 +0530

    Edit Existing products

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,24 +1,42 @@
 class ProductsController < ApplicationController
-
-  def new
-    @product = Product.new(type: params[:type])
-  end
-
-  def create
-    @product = Product.new(product_params)
-    if @product.save
-      redirect_to action: :index
-    else
-      render action: :new
-    end
-  end
+  before_action :set_product, only: [:edit, :update]
 
   def index
     @products = Product.all
   end
 
-  private
+  def new
+    @product = Product.new(type: params[:type])
+  end
 
+  def edit
+  end
+
+  def create
+    @product = Product.new(product_params)
+    if @product.save
+
+      redirect_to products_path, notice: 'Product was successfully created.'
+    else
+      render :new
+    end
+  end
+
+  def update
+    if @product.update(product_params)
+      redirect_to products_path, notice: 'Product was successfully updated.'
+    else
+      render :edit
+    end
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_product
+      @product = Product.find(params[:id])
+    end
+
+    # Never trust parameters from the scary internet, only allow the white list through.
     def product_params
       params.require(:product).permit(:type, :name, :color, :status, :inward_date, :description)
     end
