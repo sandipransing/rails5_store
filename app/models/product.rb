7d510d3832bac01dadde6f2225e97d1f9acf27d2
sandip ransing <sandip@funonrails.com>
2017-07-06 18:57:40 +0530

    Add notebook

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,9 @@
 class Product < ApplicationRecord
 
-  validates :type, :name, :inward_date, presence: true
+  TYPE_PEN = 'Pen'
+  TYPE_NOTEBOOK = 'Notebook'
+  Types = [TYPE_PEN, TYPE_NOTEBOOK]
+
+  validates :type, inclusion: { in: Types }, presence: true
+  validates :name, :inward_date, presence: true
 end
