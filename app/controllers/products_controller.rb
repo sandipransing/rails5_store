51d175c6d807b76391f965b5fbd419e522ffa613
sandip ransing <sandip@funonrails.com>
2017-07-06 17:28:25 +0530

    Add new product form

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,5 +1,25 @@
 class ProductsController < ApplicationController
+
+  def new
+    @product = Product.new
+  end
+
+  def create
+    @product = Product.new(product_params)
+    if @product.save
+      redirect_to action: :index
+    else
+      render action: :new
+    end
+  end
+
   def index
     @products = Product.all
   end
+
+  private
+
+    def product_params
+      params.require(:product).permit(:name, :color, :status, :inward_date)
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  get 'products' => 'products#index'
 
+  resources :products, only: [:new, :create, :index]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
